f1133efbd355c1bcf60f094653ac3e9134710eac
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-08-30 21:00:25 +0300

    Initial commit

---
+++ src/main/java/com/aleksandrphilimonov/bysell/BysellApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandrphilimonov.bysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BysellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BysellApplication.class, args);
+	}
+
+}
