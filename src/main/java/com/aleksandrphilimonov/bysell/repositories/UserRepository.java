a71ee4e6a6a6b09a03fb9f4206c7b51076b46d41
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-02 12:57:16 +0300

    add spring-security 2. securityConfig UserController UserService

---
+++ src/main/java/com/aleksandrphilimonov/bysell/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aleksandrphilimonov.bysell.repositories;
+
+import com.aleksandrphilimonov.bysell.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
