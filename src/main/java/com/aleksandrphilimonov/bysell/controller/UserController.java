97c83bce7ecc1d02fd4850bcde27dc98b01a74bb
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-02 13:20:19 +0300

    add spring-security 3. form login and registration

--- src/main/java/com/aleksandrphilimonov/bysell/controller/UserController.java
+++ src/main/java/com/aleksandrphilimonov/bysell/controller/UserController.java
@@ -1,15 +1,42 @@
 package com.aleksandrphilimonov.bysell.controller;
 
+import com.aleksandrphilimonov.bysell.models.User;
+import com.aleksandrphilimonov.bysell.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping("/registration")
     public String registration() {
         return "registration";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/registration")
+    public String createUser(User user, Model model) {
+        if (!userService.create(user)) {
+            model.addAttribute("errorMessage", "Пользователь с email: " + user.getEmail() + " уже существует");
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl() {
+        return "hello";
+    }
+
+
 }
